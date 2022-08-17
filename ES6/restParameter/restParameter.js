d329694f70f8364a5c65a83b04ebbd0530011fc2
MehraDevesh2022 <mehra.devesh2022@gmail.com>
2022-08-17 18:46:11 +0530

    resetParameter

---
+++ ES6/restParameter/restParameter.js
@@ -0,0 +1,19 @@
+// The rest parameter syntax allows a function to accept an indefinite number of arguments as an array, same like argumnet object but diffrent way.
+// (...) three dots are used before vairable..
+function rest(...args){
+    console.log(args);
+}
+rest(1,2,3,4,5,6,7); //output: [1,2,3,4,5,6,7] : an array
+
+// wap of of numbers passed through actual argumnets using reset parameter =>
+
+function sum(...sum){
+ // now sum is array =>
+ let total=0;
+ for(let val of sum){
+    total += val;
+ }
+  console.log(total);
+}
+// 10 arguments at one time
+sum(1,2,3,4,5,6,7,8,9,10);  // output : 55
