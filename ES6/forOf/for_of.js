e4ef807e1728f696121d1f4ae0e19d95c8a1fbc9
MehraDevesh2022 <mehra.devesh2022@gmail.com>
2022-08-17 21:41:26 +0530

    for of , in

---
+++ ES6/forOf/for_of.js
@@ -0,0 +1,78 @@
+"use strict"
+/*
+  the for of loop =>
+
+  it used over the itrable object like array , string , arguments , map so on
+ Same like for each loop in java.
+
+ syntax : => for(var name of  itrable object){
+                         // statmenet
+                 }
+
+*/
+
+let arry =[1 , 2, 3 , 4 , 5];
+
+for(let elm of arry){ // elm has every arry elm one by one. no need to terimnation condition. 
+      console.log(elm);
+}
+/*
+1
+2
+3
+4
+5
+*/
+
+let obj ={
+    name : "Rahul sinha",
+    age : 24,
+    proffesion : "boxer",
+    coach : "Devesh"
+}
+// for of not work over the object. directly error occures =>
+// for(let elm of obj){
+//   console.log(elm); // TypeError: obj.key is not iterable
+// }
+
+
+
+
+// using for in array index or key object access instead of value. 
+for(let key in obj){
+let value = obj[key];
+console.log(value); // as an array type access value with array notaion use
+}
+
+
+
+// for string =>
+let str = "hello world";
+for (let Char of str) {
+  console.log(str); 
+}
+/* 
+output : 11 time hello world print bcause lenght of str 11.
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+   
+*/
+
+
+// wap sum program using for of ===>
+
+let arr = [1 , 2, 3, 4, 5]
+let total =0;
+for(let elm of arr){
+    total += elm;
+}
+console.log(total); // output : 15
---
+++ ES6/tampletLitrel/tampletLitrel.js
@@ -0,0 +1,18 @@
+// Template Literals use back-ticks (``) rather than the quotes ("") to define a string:  aka (tilde)
+// whatever we right in text editor it will gives the output as it is. for new line no need of \n as well.
+console.log(`hello 
+world ! 
+            you are amazing`);
+            /* output:
+            hello 
+            world ! 
+                       you are amazing
+            
+            */
+
+
+// print variable value without concatintaion with String. =>
+
+let val = 2;
+
+console.log(`val saqure == ${val * 2} `); //output : val saqure == 4 
