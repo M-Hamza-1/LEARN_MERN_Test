96407957bdd8c7d3d094ead343bc5473bf08a09c
MehraDevesh2022 <mehra.devesh2022@gmail.com>
2022-09-14 00:04:47 +0530

    http

---
+++ freecodeCamp-NodeJs-expressJs/httpIntro/app.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const server = http.createServer((req,res) =>{
+  if(req.url ==='/'){
+    res.end(`
+    <h1 style ="color : red">Welcome to home page</h1>`);
+  }
+ else if(req.url === '/about'){
+    res.end("This is about page");
+  } else{
+    res.end(`
+    <h1>OOps Snap 404 !</h1>
+    <p>We can't find your page</p>
+    <a href ="/">Home Page</a>
+    `)
+  }
+
+})
+
+// listening at 3000
+server.listen(3000, function(){
+    console.log("Server start at port 3000");
+});
+
