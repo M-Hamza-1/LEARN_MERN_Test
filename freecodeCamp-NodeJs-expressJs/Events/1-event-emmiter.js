a9675773d41d9e8b209d2c4559fc7236c607b0a7
MehraDevesh2022 <mehra.devesh2022@gmail.com>
2022-09-14 18:53:31 +0530

    events

--- freecodeCamp-NodeJs-expressJs/Events/1-event-emmiter.js
+++ freecodeCamp-NodeJs-expressJs/Events/1-event-emmiter.js
@@ -1,10 +1,15 @@
 const EventEmitter = require("events"); // invoking a class
 
  const customEmitter = new EventEmitter(); // making  a instance of EventEmitter class
+
+
 // customEmitter.emit("response", mr - smilga, 34); // this will not give any outcome because. ye event listen hone se pehle use kiya ja rha hai.
+
+
 customEmitter.on("response" ,(name ,id) =>{
   console.log(`data recieved user ${name} with id:${id}`); // on is the one of the method of events module it used for to listen to events. here response is the event
 })
+
 customEmitter.on('response' , ()=>{
     console.log("this is some other logic here");
 })
