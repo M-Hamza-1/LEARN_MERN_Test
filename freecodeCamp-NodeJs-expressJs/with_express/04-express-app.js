42cfa7de55b29adbf850b461c05f9977c6d10ce6
MehraDevesh2022 <mehra.devesh2022@gmail.com>
2022-09-16 19:34:53 +0530

    all staticlly

--- freecodeCamp-NodeJs-expressJs/with_express/04-express-app.js
+++ freecodeCamp-NodeJs-expressJs/with_express/04-express-app.js
@@ -2,13 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// setup static and middleware (this  static midlleware render all page from public folder  html page src page as well and send resposne)
+// setup static and middleware (this  static midlleware render all page from public folder  those are available as html ref at the time of parsig)
 app.use(express.static("./public"));
 
-// now no all work done by app.use(express.static('./public')); afetr pittinh html file also in public folder
-// app.get('/', (req, res) =>{
-//   res.sendFile(path.resolve(__dirname, "./NavProject/index.html")); // using this path of index.html (css and js not loaded) untill  express.static('./public') not use
-// })
+
+app.get('/', (req, res) =>{
+  res.sendFile(path.resolve(__dirname, "./NavProject/index.html")); // using this path of index.html (css and js not loaded) untill  express.static('./public') not use
+})
 
 app.all("*", (req, res) => {
   res.status(404).send("<h1 style ='color : red'>404 page not found</h1>");
