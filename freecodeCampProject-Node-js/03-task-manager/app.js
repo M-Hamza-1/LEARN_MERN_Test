51746fd428c15c99707a2e2415528831f3bca192
MehraDevesh2022 <mehra.devesh2022@gmail.com>
2022-09-21 18:17:52 +0530

    mongoose schema created

--- freecodeCampProject-Node-js/03-task-manager/app.js
+++ freecodeCampProject-Node-js/03-task-manager/app.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasksRoute");
-
+const connectDB= require('./db/connect');
 app.use(express.json());
+
+
+
 app.use("/api/v1/tasks", tasks);
+
+
+
+
+
+
+// server and DB  
 const port = 3000;
-app.listen(port, console.log(`Server listening at port ${port}`));
+const start =async () =>{
+try{
+await connectDB();
+app.listen(port, console.log(`Server listening at port ${port}...`));
+} catch(err){
+    console.log(err);
+}
+} 
+start();
--- freecodeCampProject-Node-js/03-task-manager/controller/tasksController.js
+++ freecodeCampProject-Node-js/03-task-manager/controller/tasksController.js
@@ -1,11 +1,23 @@
+const TaskModel = require("../model/TasksModel");
+
+
 
 const getController = (req, res) => {
   res.send("this is home page");
 };
 
-const createController =(req , res) =>{
- res.send(req.body);
- console.log(req.body);
+const createController =async (req , res) =>{
+  try{
+  const newTask = await TaskModel.create(req.body);
+  res.status(201).json({
+  tasks: newTask,
+  msg: "new Task is created successfully",
+});
+  }catch(err){
+      res.status(501).json({
+      msg : err
+    })
+  }
 }
 
 const getControllerByID = (req, res) => {
