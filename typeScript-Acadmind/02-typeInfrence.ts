31838757efd896d8ce8cda389b1059a3cd85c38c
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-06-19 12:58:38 +0530

    refector : Core type code added

---
+++ typeScript-Acadmind/02-typeInfrence.ts
@@ -0,0 +1,33 @@
+ // basic ==>
+
+    // 1. type inference
+    
+// automatically typescript will infer the type of variable based on the value assigned to it
+ // example:
+//  let number1 = 5; // here typescript will infer the type of number1 as number because we have assigned a number value to it
+//  let number2 = 2.99; // here typescript will infer the type of number2 as number because we have assigned a number value to it
+//  let printResult = true; // here typescript will infer the type of printResult as boolean because we have assigned a boolean value to it
+//  let resultPhrase = "Result is : "; // here typescript will infer the type of resultPhrase as string because we have assigned a string value to it
+
+let person = {
+    name : "Maximilian", // here typescript will infer the type of name as string because we have assigned a string value to it
+    age : 30 ,// here typescript will infer the type of age as number because we have assigned a number value to it
+    hobbies : ["Sports" , "Cooking"] // here typescript will infer the type of hobbies as array of string because we have assigned a array of string value to it
+};
+
+console.log(person.name); 
+
+// !! Type Error !!
+// person.hobbies.push(1); // this will give error by typeScript beacuse we decalre as array of string and assign number value to it
+
+// 2. explicit type declaration
+
+// here we can explicitly declare the type of variable
+// example:
+let number3 : number; // here we have explicitly declared the type of number1 as number
+let number4 : number = 5; // here we have explicitly declared the type of number2 as number and assigned a number value to it but this is not required because typescript will infer the type of number2 as number because we have assigned a number value to it
+ 
+ // !! Type Error !!
+  // number3 = "x" // this will give error by typeScript beacuse we decalre as number and assign string value to it
+
+  
--- typeScript-Acadmind/app.ts
+++ typeScript-Acadmind/app.ts
@@ -1,18 +1,140 @@
- function add(number1: number , number2: number , showResult : boolean , phrase : string){
-    const result = number1 + number2;
-    if(showResult){
-      console.log(phrase + result); // here we are using typecasting to convert number to string 
-      
-    }else{
-      return result; 
+// core type
+
+// 1 Number : 1, 5.3, -10 any kind of number , no diference between integer or float
+// eg: 
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+let number1 :number;
+const number2 = 2;
+ number1 = 5; // only number can be assigned to number1
+
+ console.log(add(number1, number2)); // 7
+
+
+// 2 String : 'Hi', "Hi", `Hi` all text values
+// eg:
+
+function authenication(userName: string, password: string) {
+    if (userName === 'admin' && typeof password === 'string') {
+        return userName + " " + "is logged in";
     }
-   
+    return "Invalid username or password";
+}
+ 
+let userName = 'admin';
+let password : string;
+password = '123admin';
+ console.log(authenication(userName, password)); // admin is logged in
+
+ 
+ // 3 Boolean : true or false { 0 or 1 } no "truthy" or "falsy" values like in JS eg: 0, 1, '0', '1', 'true', 'false'
+    // eg:
+    function hasValue(value: boolean) {
+        return value;
+    }
+
+    let value1 : boolean;
+
+    // !! Type error !!
+    // value1 = 1; // Type '1' is not assignable to type 'boolean'. ts(2322) only true or false can be assigned to boolean no thruthy or falsy values are allowed
+
+    value1 = true;
+    console.log(hasValue(value1)); // true
+
+
+// 4 Object : {age: 30} any JS object, more specific types (type of object)
+
+// eg:
+function printEmployeeInfo(emp: {name: string, age: number}) {
+    console.log(emp.name + " " + emp.age);
+}
+
+let employee = {
+    name: 'Max',
+    age: 30
+};
+ // or
+
+//  let employee: {
+//     name: string;
+//     age: number;
+// } = { 
+//     name: 'Max',
+//     age: 30
+// }; // this is also valid
+
+printEmployeeInfo(employee); // Max 30
+
+
+// 5 Array : [1, 2, 3] any JS array, type can be flexible or strict (regarding the element types)
+
+// eg:
+function printArray(arr: string[]) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i].toUpperCase()); //  TS knows that arr[i] is a string
+    }
+}
+
+let arr = ['a', 'b', 'c'];  // all elements in the array should be string
+// or let arr: string[] = ['a', 'b', 'c']
+printArray(arr); // A B C
+
+
+// 6 Tuple : [1, 2] added by TS: Fixed-length array with fixed types
+ 
+// eg:
+function printTuple(tup: [number, string]) {
+    console.log(tup[0]);
+    console.log(tup[1]);
+
+
+}
+
+let tupleArr: [number, string];
+tupleArr = [1, 'Max'];
+ // !! Type error !!
+//  tupleArr = ['Max', 1]; // Type 'string' is not assignable to type 'number'. becuase 0th index should be number and 1st index should be string
+ // tupleAr[2] = 'abc'; // !! Type error !! size of the tuple is fixed only 2 elements can be added to the tuple
+  
+ // !! exception !!
+  tupleArr.push('abc'); // no error because push is an exception for tuple
+
+
+printTuple(tupleArr); // 1 Max
+
+
+
+// 7 Enum : enum { NEW, OLD } added by TS: Automatically enumerated global constant identifiers enum easy to assign friendly names to set of values
+
+// eg: 
+enum Role { ADMIN, READ_ONLY, AUTHOR }; // ADMIN = 0, READ_ONLY = 1, AUTHOR = 2 // default values are 0, 1, 2 but we can assign our own values
+// or enum Role {ADMIN = "ADMIN", READ_ONLY = true, AUTHOR = 1}; // we can assign our own values to the enum
+
+const person = {
+    name: 'Max',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
+
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
+}
+else if (person.role === Role.READ_ONLY) {
+    console.log('is read only');
+}
+else if (person.role === Role.AUTHOR) {
+    console.log('is author');
 }
 
 
- const number1 = 5; // infernce type of number1 is 5 because once in const we have assigned a value to it then we cannot change it so typescript will infer the type of number1 as 5 and not as number
- let number2 =2.99; // this is a float value so typescript will infer the type of number2 as number
-    const printResult = true; // this is a boolean value so typescript will infer the type of printResult as boolean
-    const resultPhrase = "Result is : "; // this is a string value so typescript will infer the type of resultPhrase as string
+// 8 Any : * avoid * any kind of value, no specific type assignment eg: let value: any; value = 5; value = 'Max'; value = true; same as JS
 
-    add(number1 , number2 , printResult , resultPhrase); 
+// eg:
+let value: any; // * avoid *
+value = 5;
+value = 'Max';
+value = true;
+console.log(value); // true // same as JS
